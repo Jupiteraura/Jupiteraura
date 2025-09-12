26a124cafc39ef113463440d25140338409ce5ef
Jupiteraura <129902949+Jupiteraura@users.noreply.github.com>
2025-09-11 21:36:29 -0400

    Add files via upload

---
+++ folks.py
@@ -0,0 +1,31 @@
+name = "aliyah collins"
+print(name.title())
+
+name = "madison mclaughlin"
+print(name.upper())
+print(name.lower())
+
+first_name = "jeremiah"
+last_name = "mclaughlin"
+full_name = f"{first_name} {last_name}"
+message = f"Wassup, {full_name.title()}"
+print(message)
+
+
+full_name = "My Kids"
+message = f"{full_name.title()} you are beautiful and smart"
+print(message)
+
+name = "Nispey Hussle"
+message = " once said The best thing you can do for a person is to insipre them,\nThats the best currency you can offer-inspisration."
+print(name + message)
+
+famous_person = "Nispey Hussle"
+message	=  " took his wildest dreams then mapped them out."
+print(famous_person + message)
+
+favorite_kid = '\n\tjermiah\n\taliyah\n\tmadison'
+favorite_kid.rstrip()
+favorite_kid.lstrip()
+favorite_kid.strip 
+print(favorite_kid.title())
